c1128481f6785a7af44eeb6df53073f2f0329b02
nirajdalmia <nirajdalmia@gmail.com>
2016-10-26 02:37:43 +0000

    Load games in order

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,7 +6,7 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.json
   def index
-    @games = Game.all
+    @games = Game.all.order("created_at DESC")
   end
 
   # GET /games/1
