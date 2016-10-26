47aadaa66a0fa6aaa63f954eee899910c8cf85f6
nirajdalmia <nirajdalmia@gmail.com>
2016-10-26 01:30:26 +0000

    games

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026010354) do
+ActiveRecord::Schema.define(version: 20161026012538) do
+
+  create_table "games", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "games", ["user_id"], name: "index_games_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
