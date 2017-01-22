7d4122589a01c643571fe5a22ca45ce793ba98bb
nirajdalmia <nirajdalmia@gmail.com>
2017-01-22 17:21:22 +0000

    added AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,9 @@ Rails.application.configure do
 # config.paperclip_defaults = {
 #  :storage => :s3,
 #  :s3_credentials => {
-#    :bucket => ENV['S3_BUCKET_NAME'],
-#    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-#    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+#    :bucket => ENV[ludi],
+#    :access_key_id => ENV[AKIAIHFDAM5AB5FSYP6A],
+#    :secret_access_key => ENV[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]
 #  }
 # }
 
