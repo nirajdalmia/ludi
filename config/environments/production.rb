7d0ba9e26fd33871e218ebf6bc576f0f5140efd2
nirajdalmia <nirajdalmia@gmail.com>
2017-01-22 17:45:14 +0000

    AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,14 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'ludi.herokuapp.com/' }
 
-# config.paperclip_defaults = {
-#  :storage => :s3,
-#  :s3_credentials => {
-#    :bucket => ENV[ludi],
-#    :access_key_id => ENV[AKIAIHFDAM5AB5FSYP6A],
-#    :secret_access_key => ENV[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]
-#  }
-# }
 
+config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('ludi'),
+    access_key_id: ENV.fetch('AKIAIHFDAM5AB5FSYP6A'),
+    secret_access_key: ENV.fetch('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'),
+    s3_region: ENV.fetch('US Standard'),
+  }
+}
 end
